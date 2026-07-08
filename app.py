411a43133de2517a11b9e7797ccc3c6630f631dd
Vince Paul Dela Cruz <vpdelacrauz@solomonic.co.uk>
2026-07-08 18:07:38 +0800

    addresses bug#1

--- app.py
+++ app.py
@@ -67,8 +67,7 @@ def rental_days(from_date, to_date):
 
 def dates_overlap(start_a, end_a, start_b, end_b):
     """True if date range A overlaps date range B."""
-    return start_b <= start_a <= end_b
-
+    return start_a <= end_b and start_b <= end_a
 
 def find_conflicting_booking(equipment_id, from_date, to_date, bookings):
     """Return an existing booking that clashes with this one, or None."""
